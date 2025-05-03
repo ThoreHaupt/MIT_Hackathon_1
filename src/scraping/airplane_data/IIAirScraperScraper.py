692b66a064f84bf0826bb4c177d775d7a7272c7c
Nils Matthäus <nils.matthaeus@ka-raceing.de>
2025-05-03 02:10:04 +0200

    added airports and flights to scraping

---
+++ src/scraping/airplane_data/IIAirScraperScraper.py
@@ -0,0 +1,7 @@
+
+class IAirScraper:
+    def __init__(self):
+        pass
+
+    def getData(self):
+        pass
--- src/scraping/scraper_api.py
+++ src/scraping/scraper_api.py
@@ -1,5 +1,7 @@
 import datetime
 import WetterEnum
+from src.scraping.airplane_data.AtlasAirScraper import AtlasAirScraper
+from src.scraping.airplane_data.getAirports import getAirportsAsDict
 from src.scraping.traffic_data.traffic_issues import traffic_issues
 
 
@@ -31,7 +33,7 @@ def get_train_stations():
     return ["Berlin", "Karlsruhe", "Hamburg"]
 
 def get_airports():
-    return ["Berlin", "Karlsruhe", "Hamburg"]
+    return getAirportsAsDict()
 
 def get_ships():
     return ["Berlin", "Karlsruhe", "Hamburg"]
@@ -50,4 +52,8 @@ def get_traffic_issues():
 
 def get_construction_data():
     return get_construction_data()
+
+def get_flights():
+    return AtlasAirScraper().getData()
+
 
