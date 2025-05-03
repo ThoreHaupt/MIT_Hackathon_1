c9c3061bd9d074022861ac7bd86cbc9ecd6b3c49
Lea Chen <leachen@Leas-MacBook-Air.local>
2025-05-03 03:31:30 -0400

    time eval

--- src/cost_eval_model/params.py
+++ src/cost_eval_model/params.py
@@ -1,6 +1,6 @@
 max_speed_train= 100 # km/h
 max_speed_ship= 20 # km/h
-max_speed_highway = 80 # km/h
+max_speed_truck = 80 # km/h
 max_speed_air = 800 # km/h
 
 PLZ_DATA_PATH = "MIT_Hackathon_1/src/cost_eval_model/traffic_pred/plz_data_total.csv"
--- src/cost_eval_model/time_cost_eval.py
+++ src/cost_eval_model/time_cost_eval.py
@@ -17,4 +17,43 @@ class TimeCostModel(CostModel):
         #           dist_mode_start_next_edge += dist
         # not same mode (for time => add 2 Stunden)
         # speed?
-        pass
+        edge_time = 0
+        dist_in_km = data["distance_next_edge"]
+        data["dist_mode_start_next_edge"] += data["time_prior_edge_end"]
+
+        if data["origin"]["mode_prior_edge"] == data["destination"]["mode_next_edge"]:
+            if data["origin"]["mode_next_edge"] == 'train':
+                edge_time = dist_in_km * 1/max_speed_train
+            elif data["origin"]["mode_next_edge"] == 'air':
+                edge_time = dist_in_km * 1/max_speed_air
+            elif data["origin"]["mode_next_edge"] == 'truck':
+                edge_time = dist_in_km * 1/max_speed_truck
+            elif data['origin']['mode_next_edge'] == "ship":
+                edge_time = dist_in_km * 1/max_speed_ship
+        else:
+            edge_time = 2
+
+        return edge_time
+
+    """
+        edge_data:
+        {
+            "origin": {
+                          "lat": 0,
+                          "lng": 0
+                          "mode_prior_edge": | "train" | "ship" | "truck" | "air"
+        },
+        "destination": {
+                           "lat": 0,
+                           "lng": 0,
+                           "mode_next_edge": | "train" | "ship" | "truck" | "air"
+        },
+        "highway": 
+                        "motorway" | "trunk" | "primary" | "secondary" | "tertiary"
+                                                                    "max_speed_next_edge": 0,
+        "distance_next_edge": 0,
+        "distance_mode_start_next_edge": 0,
+        "time_prior_edge_end": 0,
+        }
+
+        """
