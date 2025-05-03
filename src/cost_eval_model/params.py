e79af0e15fc9d797ad9c0c10bbab4b75ba311d15
Thore Haupt <thore.haupt@ka-raceing.de>
2025-05-03 07:22:25 +0200

    cost model

--- src/cost_eval_model/params.py
+++ src/cost_eval_model/params.py
@@ -4,3 +4,5 @@ max_speed_highway = 80 # km/h
 max_speed_air = 800 # km/h
 
 price_rail_per_km_per_ton = 0.28 # USD
+
+PLZ_DATA_PATH = "MIT_Hackathon_1/src/cost_eval_model/traffic_pred/plz_data_total.csv"
